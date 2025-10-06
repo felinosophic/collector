6ef1f3c6e39d083af92f0c9c8a4be6e65e6a4afa
stefanohmsml <131656754+stefanohmsml@users.noreply.github.com>
2025-10-06 21:36:49 +0200

    May I introduce, Workflow Fix the Third

--- conanfile.py
+++ conanfile.py
@@ -11,7 +11,9 @@ class CollectorRecipe(ConanFile):
        self.requires("spdlog/1.15.3")
        self.requires("gtest/1.17.0")
        self.requires("libpqxx/7.10.2")
-    
+       self.requires("openssl/3.3.1")    
+       self.requires("llama-cpp/b6565")
+
     def layout(self):
         build_folder = str(self.settings.build_type).lower()
         self.folders.build = f"build/{build_folder}"
